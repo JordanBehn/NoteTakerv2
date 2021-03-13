0337b27ae7595d6a51c5a697e756ad7e9e605183
JQB <jordanqbehn@gmail.com>
2021-03-12 21:45:40 -0600

    adjusted delete route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,23 +5,27 @@ const db = require("../db/db.json");
 //api routes
 module.exports = function (app) {
   //get returns db.json as a json
+  //THERES A BUG HERE! DELETE IS SUCCESSFULLY MODIFYING THE DB.JSON, BUT THE PAGE IS NOT UPDATING CORRECTLY. I AM LIKELY NOT GRABBING THE UPDATED JSON ON UPDATE
   app.get("/api/notes", function (req, res) {
+    //const db = require("../db/db.json");
+
     res.json(db);
   });
   // creates a new note, pushes new note to list of notes, writes new list to db.json
   app.post("/api/notes", function (req, res) {
+    //const db = require("../db/db.json");
     //new note contains id generated its place in the list
     var newNote = {
       id: db.length,
       title: req.body.title,
       text: req.body.text,
     };
-    console.log(newNote)
+    console.log(newNote);
 
     db.push(newNote);
 
     //write new db to json
-    fs.writeFile("../db/db.json", JSON.stringify(db), (err) => {
+    fs.writeFile("db/db.json", JSON.stringify(db), (err) => {
       if (err) throw err;
     });
 
@@ -29,12 +33,18 @@ module.exports = function (app) {
   });
 
   //delete note with input id
-  app.delete("/api/notes:id", function (req, res) {
+  app.delete("/api/notes/:id", function (req, res) {
     const delID = req.params.id;
-    console.log(delID)
+    console.log(delID);
     console.log("params", req.params);
-    db.splice(delID, 1);
-    res.json(db);
-  });
+    const filtereddb = db.filter((note) => `${note.id}` !== delID);
 
+    console.log(filtereddb);
+    fs.writeFile("db/db.json", JSON.stringify(filtereddb), (err) => {
+      console.log(err);
+      if (err) throw err;
+    });
+
+    res.json(filtereddb);
+  });
 };
